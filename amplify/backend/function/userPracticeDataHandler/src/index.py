944ed9fbaadd357e3dd41b33e88fe90e34d1a673
Izaan Aamir <65916412+izaanaamir@users.noreply.github.com>
2024-04-03 03:13:47 +0200

    fix: corrected the missing RETURN statement in lambda trigger

--- amplify/backend/function/userPracticeDataHandler/src/index.py
+++ amplify/backend/function/userPracticeDataHandler/src/index.py
@@ -13,7 +13,7 @@ def handler(event, context):
     resource = event["resource"]
 
     if http_method == "GET" and resource == "/user/{user-id}/practice-data-scores":
-        handle_get_request(event)
+        return handle_get_request(event)
 
     else:
         return {"statusCode": 405, "body": json.dumps("Method Not Allowed")}
@@ -30,27 +30,17 @@ def handle_get_request(event):
         response = table.get_item(Key={"userId": user_id})
         user_data = response.get("Item", {})
         if user_data:
-            user_data = {
-                "userId": user_data.get("userId").get("S"),
-                "confidenceScore": user_data.get("confidenceScore").get("N"),
-                "clarityScore": user_data.get("clarityScore").get("N"),
-                "fillerWordUsage": user_data.get("fillerWordUsage").get("N"),
-                "emotionalScore": user_data.get("emotionalScore").get("N"),
-                "attentionScore": user_data.get("attentionScore").get("N"),
+            return {
+                "statusCode": 200,
+                "body": json.dumps(user_data),
             }
-
-            body = json.dumps(user_data)
-            status_code = 200
-    except Exception as e:
-        body = json.dumps({"error": str(e)})
-        status_code = 500
-
-    return {
-        "statusCode": status_code,
-        "headers": {
-            "Access-Control-Allow-Headers": "*",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-        },
-        "body": body,
-    }
+        else:
+            return {
+                "statusCode": 404,
+                "body": json.dumps({"error": "User not found"}),
+            }
+    except Exception:
+        return {
+            "statusCode": 500,
+            "body": json.dumps("Error in uploading file."),
+        }
